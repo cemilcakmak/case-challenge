cd90684d2fe29e13f2ebc9b213a60425df0d3893
cemÄilcakmak <b.cemilcakmak@gmail.com>
2022-01-15 13:20:43 +0300

    made some updates and commented the classes

--- case-challenge/app.js
+++ case-challenge/app.js
@@ -12,13 +12,7 @@ const keysRouter = require('./routes/keys-router');
 const endpointIsNotFound = require('./middleware/not-found');
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Initial Endpoint For Getir Case Study Challenge.');
-});
-
 keysRouter(app);
-
 app.use(endpointIsNotFound);
 
 const port = process.env.PORT || 4000;
@@ -27,7 +21,6 @@ const start = async () => {
         await connectDB(process.env.MONGO_URI);
         app.listen(port, console.log(`Server is listening at port ${port}`));
     } catch (error) {
-        // will be replaced
         console.log(error);
     }
 }
--- case-challenge/controllers/keys-controller.js
+++ case-challenge/controllers/keys-controller.js
@@ -1,9 +1,17 @@
+/**
+ * Controller class to manage request data.
+ * Checks if request is suitable to make operations in service class
+ */
+
 // npm packages
 const StatusCodes = require('http-status-codes')
 
 // project classes
 const { Utils } = require('../utils');
 const { KeysService } = require('../services/keys-service');
+
+// enum
+const ResponseCodes = require('../response-codes');
 
 class KeysController {
     constructor(opts) {
@@ -12,16 +20,25 @@ class KeysController {
     async getKeys(req, res) {
         try {
             let requestPayload = req.body;
+            let startDate = requestPayload.startDate;
+            let endDate = requestPayload.endDate;
+            let minCount = requestPayload.minCount;
+            let maxCount = requestPayload.maxCount;
 
             // Check if parameters in the request body are assigned or not
             let isRequestValid = Utils.areRequestParametersAssigned({
-                startDate: requestPayload.startDate,
-                endDate: requestPayload.endDate,
-                minCount: requestPayload.minCount,
-                maxCount: requestPayload.maxCount
+                startDate: startDate,
+                endDate: endDate,
+                minCount: minCount,
+                maxCount: maxCount
             });
             if (!isRequestValid) {
-                return res.status(StatusCodes.BAD_REQUEST).json({ msg: 'Bad Request: Request Parameters are not assigned.' });
+                return res.status(StatusCodes.BAD_REQUEST)
+                .send({
+                    code: ResponseCodes.BAD_REQUEST.code,
+                    msg: ResponseCodes.BAD_REQUEST.msg,
+                    keys: null
+                });
             }
 
             // Check if date values are match with YYYY-MM-DD format
@@ -30,20 +47,48 @@ class KeysController {
                 endDate: requestPayload.endDate
             });
             if (!areDateParametersValid) {
-                return res.status(StatusCodes.BAD_REQUEST).json({ msg: 'Bad Request: Date formats are not supported.'});
+                return res.status(StatusCodes.BAD_REQUEST)
+                    .send({
+                        code: ResponseCodes.BAD_REQUEST.code,
+                        msg: ResponseCodes.BAD_REQUEST.msg,
+                        keys: null
+                    });
             }
             
             let keysService = new KeysService();
             let keys = await keysService.getKeysResponse(requestPayload);
 
-            let response = {
-                code: 0,
-                msg: 'Success',
-                keys
+            if (!Utils.assigned(keys)) {
+                return res.status(StatusCodes.BAD_REQUEST)
+                    .send({
+                        code: ResponseCodes.INTERNAL_SERVER_ERROR.code,
+                        msg: ResponseCodes.INTERNAL_SERVER_ERROR.msg,
+                        keys: null
+                    });
             }
-            res.status(StatusCodes.OK).send(response);
+            
+            if (keys.length == 0) {
+                return res.status(StatusCodes.NOT_FOUND)
+                    .send({
+                        code: ResponseCodes.NO_DATA_FOUND.code,
+                        msg: ResponseCodes.NO_DATA_FOUND.msg,
+                        keys: keys
+                });
+            }
+    
+            return res.status(StatusCodes.OK)
+                .send({
+                    code: ResponseCodes.SUCCESS.code,
+                    msg: ResponseCodes.SUCCESS.msg,
+                    keys: keys
+            });
         } catch (error) {
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Internal Server Error: Something went wrong.' });
+            return res.status(StatusCodes.BAD_REQUEST)
+            .send({
+                code: ResponseCodes.INTERNAL_SERVER_ERROR.code,
+                msg: ResponseCodes.INTERNAL_SERVER_ERROR.msg,
+                keys: null
+            });
         }
     };
 
