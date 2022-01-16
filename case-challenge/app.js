33e435329405cc169d544fab88db91b1ba155a86
cemÄilcakmak <b.cemilcakmak@gmail.com>
2022-01-16 10:29:00 +0300

    updated some points on router side

--- case-challenge/app.js
+++ case-challenge/app.js
@@ -1,24 +1,26 @@
 require('dotenv').config();
 
+const MONGODB_URI = 'mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true';
+
 const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
 
 // Getir Case Challenge Router
-const keysRouter = require('./routes/keys-router');
+const challengeRouter = require('./routes/challenge-router');
 
 // Exception Handling
 const endpointIsNotFound = require('./middleware/not-found');
 
 app.use(express.json());
-keysRouter(app);
+challengeRouter(app);
 app.use(endpointIsNotFound);
 
 const port = process.env.PORT || 4000;
 const start = async () => {
     try {
-        await connectDB('mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true');
+        await connectDB(MONGODB_URI);
         app.listen(port, console.log(`Server is listening at port ${port}`));
     } catch (error) {
         console.log(error);
--- case-challenge/routes/challenge-router.js
+++ case-challenge/routes/challenge-router.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const router = express.Router();
 
 const { KeysController } = require('../controllers/keys-controller');
 
 function init(app) {
-    app.use('/get-keys', (req, res) => new KeysController().getKeys(req, res));
+    app.use('/challenge/get-keys', (req, res) => new KeysController().getKeys(req, res));
 }
 
 module.exports = init;
