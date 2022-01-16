60e8efdeb691da017363e0ab8bad9be22295cec0
cemÄilcakmak <b.cemilcakmak@gmail.com>
2022-01-16 11:22:48 +0300

    changed endpoint into http post

--- case-challenge/routes/challenge-router.js
+++ case-challenge/routes/challenge-router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { KeysController } = require('../controllers/keys-controller');
 
 function init(app) {
-    app.use('/challenge/get-keys', (req, res) => new KeysController().getKeys(req, res));
+    app.post('/challenge/get-keys', (req, res) => new KeysController().getKeys(req, res));
 }
 
 module.exports = init;
